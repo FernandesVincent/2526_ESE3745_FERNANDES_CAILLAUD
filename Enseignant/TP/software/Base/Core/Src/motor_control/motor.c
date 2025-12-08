ec25c074091ed7f66b01ac14fc66e2d654b35dce
CaillaudPaul <pcaillaud0709@gmail.com>
2025-12-08 18:31:15 +0100

    fin de seance

--- Enseignant/TP/software/Base/Core/Src/motor_control/motor.c
+++ Enseignant/TP/software/Base/Core/Src/motor_control/motor.c
@@ -19,41 +19,40 @@ void PWM_init(void){
 	HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_2);
 
 	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 0.3*16998);
-	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 0.3*16998);
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, MAX_PWM - 0.3*16998);
 
 }
 
-void SET_PWM(h_shell_t* h_shell, int argc, char** argv)
-{
+void SET_PWM(h_shell_t* h_shell, int argc, char** argv){
+
 	int size;
+	int old_PWM = 5800; //30% pwm max
 
 	if(argc!=2){
 			size = snprintf(h_shell->print_buffer, SHELL_PRINT_BUFFER_SIZE, "Need 2 arguments : motor value\r\n");
 			h_shell->drv.transmit(h_shell->print_buffer, size);
 			return HAL_ERROR;
 		}
-	if (atoi(argv[1]) > 100)
-	{
+	if (atoi(argv[1]) > 100){
 		size = snprintf(h_shell->print_buffer, SHELL_PRINT_BUFFER_SIZE, "The PWM has to be between 0 and 100% \r\n");
 		h_shell->drv.transmit(h_shell->print_buffer, size);
 		return HAL_ERROR;
 	}
 
-	else
-	{
+	else{
 		int PWM =(int) (atoi(argv[1]) * MAX_PWM / 100.0);
-		if (PWM > MAX_PWM || PWM < 0)
-			{
+		if (PWM > MAX_PWM || PWM < 0){
 				while(1);
 			}
-		else
-			{
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1,(int) PWM);
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2,(int) MAX_PWM - PWM);
-				size = snprintf(h_shell->print_buffer, SHELL_PRINT_BUFFER_SIZE, "The PWM has been set at %d %\r\n",PWM);
-				h_shell->drv.transmit(h_shell->print_buffer, size);
-				return HAL_ERROR;
-			}
+		else{
+			update_PWM_ramp(old_PWM,(int) PWM);
+//			__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1,(int) PWM);
+//			__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2,(int) MAX_PWM - PWM);
+			size = snprintf(h_shell->print_buffer, SHELL_PRINT_BUFFER_SIZE, "The PWM has been set at %d %\r\n",PWM);
+			h_shell->drv.transmit(h_shell->print_buffer, size);
+			old_PWM = PWM;
+			return HAL_ERROR;
+		}
 	}
 }
 
@@ -61,20 +60,26 @@ void ccr_init(){
 	return shell_add(&hshell1, "SETCCR", SET_PWM, "SET PWM MOTOR");
 }
 
-void update_PWM_ramp(void){
-    if(current_PWM < target_PWM){
-        current_PWM += PWM_STEP;
-        if(current_PWM > target_PWM) current_PWM = target_PWM;
-    }
-    else if(current_PWM > target_PWM){
-        current_PWM -= PWM_STEP;
-        if(current_PWM < target_PWM) current_PWM = target_PWM;
-    }
-    else{
-        return; // rien à faire
-    }
+void update_PWM_ramp(int current_PWM, int target_PWM){
+	int PWM_STEP = 850; //5% of 17000
 
-    __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, current_PWM);
-    __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, MAX_PWM - current_PWM);
+	while (current_PWM != target_PWM){
+		if(current_PWM < target_PWM){
+			current_PWM += PWM_STEP;
+			if(current_PWM > target_PWM) current_PWM = target_PWM;
+			HAL_Delay(1);
+		}
+		else if(current_PWM > target_PWM){
+			current_PWM -= PWM_STEP;
+			if(current_PWM < target_PWM) current_PWM = target_PWM;
+			HAL_Delay(1);
+		}
+		else{
+			return; // rien à faire
+		}
+
+		__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, current_PWM);
+		__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, MAX_PWM - current_PWM);
+	}
 }
 
